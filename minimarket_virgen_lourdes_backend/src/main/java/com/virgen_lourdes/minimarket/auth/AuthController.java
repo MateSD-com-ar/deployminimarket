909a3440f7f59960ebc6095ee1160d99f4d2c648
Leandro <tucholanza@gmail.com>
2024-09-23 20:30:42 -0300

    backend add front

---
+++ minimarket_virgen_lourdes_backend/src/main/java/com/virgen_lourdes/minimarket/auth/AuthController.java
@@ -0,0 +1,30 @@
+package com.virgen_lourdes.minimarket.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Validated @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "Deploy update correctly!! Don't touch the github actions please. Understand? LCDM";
+    }
+
+}
